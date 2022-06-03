9e738b5731dc5068dc0f566939592645ebaa0a27
amyspencerproject <amy@spencerproject.com>
2022-06-03 08:42:47 -0600

    Initial commit

---
+++ spec/what_should_i_eat_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WhatShouldIEat do
+  it "has a version number" do
+    expect(WhatShouldIEat::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
