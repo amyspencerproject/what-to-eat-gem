9ac3d392e38c587401494acc1e7471f9f61ba380
amyspencerproject <amy@spencerproject.com>
2022-06-03 19:12:01 -0600

    Refactored my code to be more like the solution

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -1,22 +1,22 @@
 # frozen_string_literal: true
 
 require_relative "what_should_i_eat/version"
+require 'http'
+require 'json'
 
 module WhatShouldIEat
-  class Error < StandardError; end
-  
-  require 'http'
-  result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
 
-  require 'json'
-  recipes = JSON.parse(result)
+  class Error < StandardError; end
 
   class Recipe 
     def self.getRecipe
+      result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
+      recipes = JSON.parse(result)
       title = recipes.first['items'].first['label']
       url = recipes.first['items'].first['url']
       puts title
       puts url
     end
+    # puts getRecipe
   end
 end
