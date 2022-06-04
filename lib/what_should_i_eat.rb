aa913ac3230aa8df3a96c62ca1102a21bac46338
amyspencerproject <amy@spencerproject.com>
2022-06-03 18:35:07 -0600

    Wrote method to get data, convert to json, and return recipe to user

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -4,5 +4,19 @@ require_relative "what_should_i_eat/version"
 
 module WhatShouldIEat
   class Error < StandardError; end
-  # Your code goes here...
+  
+  require 'http'
+  result = HTTP.get('https://www.edamam.com/search?type=Feeds').to_s
+
+  require 'json'
+  recipes = JSON.parse(result)
+
+  class Recipe 
+    def self.getRecipe
+      title = recipes.first['items'].first['label']
+      url = recipes.first['items'].first['url']
+      puts title
+      puts url
+    end
+  end
 end
